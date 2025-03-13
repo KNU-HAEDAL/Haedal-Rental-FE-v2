e93370b096181beda1186ff4e3d0c2f0d2e7ccb0
Dobbymin <gmin990624@gmail.com>
2025-03-14 02:41:10 +0900

    feat(admin): 재사용 가능한 Item-field 구현

---
+++ src/features/admin/components/ItemField.tsx
@@ -0,0 +1,29 @@
+import { ControllerRenderProps, useFormContext } from 'react-hook-form';
+
+import { FormControl, FormField, FormItem, FormMessage } from '@/shared';
+
+type Props = {
+  name: string;
+  label?: string;
+  children: (field: ControllerRenderProps) => React.ReactNode;
+};
+export const ItemField = ({ name, label, children }: Props) => {
+  const form = useFormContext();
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <div className='flex flex-col gap-3'>
+            <p>{label}</p>
+            <FormControl>{children(field)}</FormControl>
+          </div>
+          <div className='flex justify-start'>
+            <FormMessage />
+          </div>
+        </FormItem>
+      )}
+    />
+  );
+};
