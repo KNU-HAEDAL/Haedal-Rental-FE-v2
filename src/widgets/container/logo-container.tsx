ecaabd6c3e2e5a97b8f65e7621401ab2ebee8c82
Dobbymin <gmin990624@gmail.com>
2025-03-13 20:09:49 +0900

    refactor: 상하 padding 값 추가

--- src/widgets/container/logo-container.tsx
+++ src/widgets/container/logo-container.tsx
@@ -10,7 +10,7 @@ export const LogoContainer = ({ children, title }: Props) => {
     <div className='flex flex-col items-center justify-center h-full gap-3 text-center'>
       <SymbolLogo />
       <AuthButtons />
-      <div className='flex h-full w-[550px] flex-col items-center gap-5 border border-gray-200'>
+      <div className='flex h-full w-[550px] flex-col items-center gap-5 border border-gray-200 pb-5'>
         <div className='flex justify-center w-full py-3 bg-haedal'>
           <p className='text-xl font-bold text-white'>{title}</p>
         </div>
