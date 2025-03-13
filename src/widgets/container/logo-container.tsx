5b71ec762d54889decad497e4ec25c90f5118bff
Dobbymin <gmin990624@gmail.com>
2025-03-13 15:05:33 +0900

    feat: 재사용 가능한 container 구현

---
+++ src/widgets/container/logo-container.tsx
@@ -0,0 +1,21 @@
+import { AuthButtons, SymbolLogo } from '@/shared';
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+};
+
+export const LogoContainer = ({ children, title }: Props) => {
+  return (
+    <div className='flex flex-col items-center justify-center h-full gap-3 text-center'>
+      <SymbolLogo />
+      <AuthButtons />
+      <div className='flex h-full w-[550px] flex-col items-center gap-5 border border-gray-200'>
+        <div className='flex justify-center w-full py-3 bg-haedal'>
+          <p className='text-xl font-bold text-white'>{title}</p>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
