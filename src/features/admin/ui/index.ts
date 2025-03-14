08b5915e936236bdbb8ae2196cdbd886f6487bb4
Dobbymin <gmin990624@gmail.com>
2025-03-14 14:03:35 +0900

    feat(admin): 관리자 페이지 내부 컴포넌트 구현

--- src/features/admin/ui/index.ts
+++ src/features/admin/ui/index.ts
@@ -1,2 +1,7 @@
 export * from './table-container';
 export * from './add-item-form';
+export * from './lender-info-section';
+export * from './item-name-section';
+export * from './item-type-section';
+export * from './item-period-section';
+export * from './item-image-section';
---
+++ src/features/admin/ui/item-period-section.tsx
@@ -0,0 +1,31 @@
+import { Calendar } from 'lucide-react';
+
+import { getAfterDays } from '@/shared';
+
+import { ItemWrapper } from '../components';
+
+type Props = {
+  startDate: string;
+  endDate: string;
+};
+
+export const ItemPeriodSection = ({ startDate, endDate }: Props) => {
+  const returnDate = getAfterDays(7);
+
+  return (
+    <ItemWrapper className='gap-1 border py-5'>
+      <span className='flex w-full'>
+        물품 대여 기간을 작성해주세요. 최대 대여기간은&nbsp;
+        <p className='font-bold'>일주일</p>
+        입니다.
+      </span>
+      <p>오늘 물품 대여자의 반납 기한은 {returnDate} 입니다.</p>
+      <div className='flex w-full items-center gap-5 py-3'>
+        <Calendar size='24' />
+        <p>
+          {startDate} ~ {endDate}
+        </p>
+      </div>
+    </ItemWrapper>
+  );
+};
