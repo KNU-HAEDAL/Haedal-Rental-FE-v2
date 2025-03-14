a5c630d27639294830288faad6b01670d13a5162
joojjang <itchu999@naver.com>
2025-03-14 22:42:15 +0900

    fix(login): 이미지 파일 이름 및 경로 수정

--- src/shared/components/features/simple-logo/SimpleLogo.tsx
+++ src/shared/components/features/simple-logo/SimpleLogo.tsx
@@ -4,7 +4,7 @@ export const SimpleLogo = () => {
   return (
     <Link to='/' className='absolute top-6'>
       <img
-        src='/logo/simple-logo.svg'
+        src='/src/shared/_assets/logo/simple-logo.svg'
         alt='haedal'
         className='cursor-pointer'
         width={230}
