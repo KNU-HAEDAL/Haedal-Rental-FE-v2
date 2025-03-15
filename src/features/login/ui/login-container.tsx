2ce90215966aae6c3f531147bdd34c23c3903dc5
joojjang <itchu999@naver.com>
2025-03-15 23:35:55 +0900

    refactor(login-container): form field들 map 함수로 묶기

--- src/features/login/ui/login-container.tsx
+++ src/features/login/ui/login-container.tsx
@@ -17,17 +17,17 @@ type FormData = {
 export const LoginContainer = () => {
   const form = useForm<FormData>({ mode: 'onChange' });
 
-  const { handleSubmit, formState } = form;
+  const { control, handleSubmit, formState } = form;
+
+  const inputFields = [
+    { name: 'id', label: '아이디', type: 'text' },
+    { name: 'password', label: '비밀번호', type: 'password' },
+  ];
 
   const onSubmit = (data: FormData) => {
     // Todo: API 요청
     console.log(data);
   };
-
-  // const inputFields = [
-  //   { name: 'id', label: '아이디', type: 'text' },
-  //   { name: 'password', label: '비밀번호', type: 'password' },
-  // ];
 
   return (
     <div className='flex w-96 flex-col gap-8 rounded-xl bg-white py-6 text-black'>
@@ -35,40 +35,27 @@ export const LoginContainer = () => {
       <Form {...form}>
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-8'>
           <ul className='flex flex-col gap-3 px-8'>
-            {/* {inputFields.map(({ name, label, type }) => ( */}
-            <FormField
-              name='id'
-              render={({ field }) => (
-                <FormItem>
-                  <label className='text-sm font-semibold'>아이디</label>
-                  <FormControl>
-                    <input
-                      type='text'
-                      {...field}
-                      className='w-full rounded-lg border px-3 py-2 text-sm'
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <label className='text-sm font-semibold'>비밀번호</label>
-                  <FormControl>
-                    <input
-                      type='password'
-                      {...field}
-                      className='w-full rounded-lg border px-3 py-2 text-sm'
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            {/* ))}  */}
+            {inputFields.map(({ name, label, type }) => (
+              <FormField
+                key={name}
+                control={control}
+                name={name as keyof FormData}
+                rules={{ required: `${label}를 입력해주세요.` }}
+                render={({ field }) => (
+                  <FormItem className='flex flex-col items-start'>
+                    <label className='text-sm font-semibold'>{label}</label>
+                    <FormControl>
+                      <input
+                        type={type}
+                        {...field}
+                        className='w-full rounded-lg border px-3 py-2 text-sm'
+                      />
+                    </FormControl>
+                    <FormMessage className='text-xs' />
+                  </FormItem>
+                )}
+              />
+            ))}
           </ul>
           <div className='w-full px-8'>
             <button
