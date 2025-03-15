a21355217059eb71fd779042f5df50218d53cb9b
joojjang <itchu999@naver.com>
2025-03-15 23:23:36 +0900

    refactor(login-container): Form 모듈 사용하여 리팩토링

--- src/features/login/ui/login-container.tsx
+++ src/features/login/ui/login-container.tsx
@@ -1,60 +1,84 @@
 import { useForm } from 'react-hook-form';
 
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/shared';
+
 type FormData = {
   id: string;
   password: string;
 };
 
 export const LoginContainer = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { isValid, errors },
-  } = useForm<FormData>({ mode: 'onChange' });
+  const form = useForm<FormData>({ mode: 'onChange' });
 
   const onSubmit = (data: FormData) => {
     // Todo: API 요청
     console.log(data);
   };
 
-  const inputFields = [
-    { name: 'id', label: '아이디', type: 'text' },
-    { name: 'password', label: '비밀번호', type: 'password' },
-  ];
+  // const inputFields = [
+  //   { name: 'id', label: '아이디', type: 'text' },
+  //   { name: 'password', label: '비밀번호', type: 'password' },
+  // ];
 
   return (
     <div className='flex w-96 flex-col gap-8 rounded-xl bg-white py-6 text-black'>
       <h1 className='px-8 text-xl font-semibold'>로그인</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-8'>
-        <ul className='flex flex-col gap-3 px-8'>
-          {inputFields.map(({ name, label, type }) => (
-            <li key={name} className='flex flex-col items-start gap-2'>
-              <label className='text-sm font-semibold'>{label}</label>
-              <input
-                type={type}
-                {...register(name as keyof FormData, {
-                  required: `${label}를 입력해주세요.`,
-                })}
-                className='w-full rounded-lg border px-3 py-2 text-sm'
-              />
-              {errors[name as keyof FormData] && (
-                <p className='text-xs text-red-500'>
-                  {errors[name as keyof FormData]?.message}
-                </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col gap-8'
+        >
+          <ul className='flex flex-col gap-3 px-8'>
+            {/* {inputFields.map(({ name, label, type }) => ( */}
+            <FormField
+              name='id'
+              render={({ field }) => (
+                <FormItem>
+                  <label className='text-sm font-semibold'>아이디</label>
+                  <FormControl>
+                    <input
+                      type='text'
+                      {...field}
+                      className='w-full rounded-lg border px-3 py-2 text-sm'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </li>
-          ))}
-        </ul>
-        <div className='w-full px-8'>
-          <button
-            type='submit'
-            disabled={!isValid}
-            className={`w-full rounded-lg border px-4 py-2 text-sm font-semibold ${!isValid ? 'cursor-not-allowed bg-gray-200 text-gray-500' : 'bg-[#0F1729] text-white'} `}
-          >
-            로그인
-          </button>
-        </div>
-      </form>
+            />
+            <FormField
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <label className='text-sm font-semibold'>비밀번호</label>
+                  <FormControl>
+                    <input
+                      type='password'
+                      {...field}
+                      className='w-full rounded-lg border px-3 py-2 text-sm'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* ))}  */}
+          </ul>
+          <div className='w-full px-8'>
+            <button
+              type='submit'
+              disabled={!form.formState.isValid}
+              className={`w-full rounded-lg border px-4 py-2 text-sm font-semibold ${
+                !form.formState.isValid
+                  ? 'cursor-not-allowed bg-gray-200 text-gray-500'
+                  : 'bg-[#0F1729] text-white'
+              }`}
+            >
+              로그인
+            </button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
