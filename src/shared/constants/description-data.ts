cae497f4e8d4783d9644ca333a1ae9ca57fe0124
Dobbymin <gmin990624@gmail.com>
2025-03-18 17:47:38 +0900

    feat: 페이지 설명 data 추가

---
+++ src/shared/constants/description-data.ts
@@ -0,0 +1,18 @@
+export const DESCRIPTION_DATA = [
+  {
+    description: '해달 동아리방 물품 목록입니다.',
+    key: 'search',
+  },
+  {
+    description: '',
+    key: 'write',
+  },
+  {
+    description: '님이 대여한 물품 목록입니다.',
+    key: 'my-rent',
+  },
+  {
+    description: '해달 동아리방 물품을 관리합니다.',
+    key: 'admin',
+  },
+] as const;
