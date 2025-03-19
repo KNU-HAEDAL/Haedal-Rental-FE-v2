9d5718ad39cb3e03289ae49d1c2e5a9cc61fb5d7
joojjang <itchu999@naver.com>
2025-03-20 01:00:15 +0900

    style(AdminPage): 페이지 제목을 데이터 가져와서 사용

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -1,13 +1,13 @@
 import { TableContainer } from '@/features';
 
-import { BackButton } from '@/shared';
+import { BackButton, NAVIGATE_BUTTONS_DATA } from '@/shared';
 
 import { LogoContainer } from '@/widgets';
 
 export const AdminPage = () => {
   return (
     <div className='flex h-full flex-col items-center justify-center gap-3 text-center'>
-      <LogoContainer title='관리자 페이지'>
+      <LogoContainer title={NAVIGATE_BUTTONS_DATA[3].title}>
         <TableContainer />
       </LogoContainer>
       <div className='w-full pt-2 pb-6'>
