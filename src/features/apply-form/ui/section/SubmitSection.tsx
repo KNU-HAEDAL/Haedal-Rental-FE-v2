07476a371030c82ac9ba73a971c33d2b7a92d2fb
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-03-23 13:21:37 +0900

    Update src/features/apply-form/ui/section/SubmitSection.tsx
    Co-authored-by: Minjoo Kim <itchu999@naver.com>

--- src/features/apply-form/ui/section/SubmitSection.tsx
+++ src/features/apply-form/ui/section/SubmitSection.tsx
@@ -9,24 +9,24 @@ export const SubmitSection = () => {
   return (
     <div className='flex flex-col gap-3'>
       <ContentsContainer title='물품 대여 장부'>
-        <div className='flex flex-col gap-5 bg-white p-2 text-center leading-relaxed'>
-          <span>
+        <DescriptionSection>
+          <div>
             <p>물품 대여 장부가 정상적으로 제출되었습니다.</p>
             <p>
               원활한 물품 대여를 위해 <b>반납 기한을 준수</b>해주세요.
             </p>
-          </span>
-          <span>
+          </div>
+          <div>
             <p>
               대여한 물품은 해달의 소중한 재산입니다. 2차 공유 및 파손을
               자제해주세요.
             </p>
-          </span>
-          <p>
+          </div>
+          <div>
             물품 대여 관련 문의는 해구르르에게 개인적으로 연락해주시면
             감사하겠습니다.
-          </p>
-        </div>
+          </div>
+        </DescriptionSection>
       </ContentsContainer>
       <div className='w-full'>
         <Button
