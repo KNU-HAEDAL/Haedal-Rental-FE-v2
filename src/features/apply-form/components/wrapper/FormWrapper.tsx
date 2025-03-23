35a0745bd0cb48713efea2a54178ebf0c33532c0
Dobbymin <gmin990624@gmail.com>
2025-03-23 12:54:42 +0900

    remove(apply-form): 사용하지 않는 컴포넌트 제거

--- src/features/apply-form/components/wrapper/FormWrapper.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-};
-
-export const FormWrapper = ({ children }: Props) => {
-  return (
-    <div className='overflow-hidden border border-gray-200'>{children}</div>
-  );
-};
