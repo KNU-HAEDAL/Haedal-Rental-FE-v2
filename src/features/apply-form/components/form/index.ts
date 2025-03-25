6d89b8e9bff72c83285be57d32c16a9a98300de0
Dobbymin <gmin990624@gmail.com>
2025-03-25 13:58:50 +0900

    feat(apply-form): 폼 제출 기능 구현

--- src/features/apply-form/components/form/index.ts
+++ src/features/apply-form/components/form/index.ts
@@ -1,3 +1,4 @@
 export * from './ItemNameField';
 export * from './CategorySelectField';
 export * from './RentalPeriodField';
+export * from './ImageUploadField';
--- src/features/apply-form/ui/form/ItemApplyForm.tsx
+++ src/features/apply-form/ui/form/ItemApplyForm.tsx
@@ -1,18 +1,35 @@
+// import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 
-import { Form } from '@/shared';
+import { Button, Form } from '@/shared';
 import { ContentsContainer } from '@/widgets';
 
 import {
   CategorySelectField,
+  ImageUploadField,
   ItemNameField,
   RentalPeriodField,
 } from '../../components';
+import { useGoStep } from '../../hooks';
 import { ApplyForm, ApplyFormSchema } from '../../model';
 
-export const ItemApplyForm = () => {
+type Props = {
+  step: number;
+  setStep: (step: number) => void;
+};
+
+export const ItemApplyForm = ({ step, setStep }: Props) => {
+  const { goNextStep, goPrevStep } = useGoStep({ step, setStep });
+  // const [isValid, setIsValid] = useState({
+  //   itemName: false,
+  //   category: false,
+  //   rentalStartDate: false,
+  //   rentalEndDate: false,
+  //   itemImage: false,
+  // });
+
   const form = useForm<ApplyForm>({
     resolver: zodResolver(ApplyFormSchema),
     defaultValues: {
@@ -20,15 +37,42 @@ export const ItemApplyForm = () => {
       category: '',
       rentalStartDate: '',
       rentalEndDate: '',
-      images: [],
+      itemImage: [],
     },
   });
+
+  const onSubmit = () => {
+    try {
+      // if (!isValid.itemName) throw new Error('물품 이름을 입력해주세요.');
+      // if (!isValid.category) throw new Error('카테고리를 선택해주세요.');
+      // if (!isValid.rentalStartDate)
+      //   throw new Error('대여 시작 날짜를 선택해주세요.');
+      // if (!isValid.rentalEndDate)
+      //   throw new Error('대여 종료 날짜를 선택해주세요.');
+      // if (!isValid.itemImage) throw new Error('이미지를 업로드해주세요.');
+      // const { itemName, category, rentalStartDate, rentalEndDate, itemImage } = form.getValues();
+    } catch (error) {
+      if (error instanceof Error) {
+        // if (
+        //   !isValid.itemName ||
+        //   !isValid.category ||
+        //   !isValid.rentalStartDate ||
+        //   !isValid.rentalEndDate ||
+        //   !isValid.itemImage
+        // ) {
+        //   alert(error.message);
+        // }
+        alert(error.message);
+      }
+      goNextStep();
+    }
+  };
 
   return (
     <Form {...form}>
       <form
-        className='w-layout flex flex-col items-center gap-5'
         onSubmit={(e) => e.preventDefault()}
+        className='w-layout flex flex-col items-center gap-5'
       >
         <ContentsContainer
           className='mb-0 text-start'
@@ -37,8 +81,27 @@ export const ItemApplyForm = () => {
           <ItemNameField name='itemName' />
         </ContentsContainer>
         <CategorySelectField name='category' />
-        <RentalPeriodField />
+        <RentalPeriodField name='rentalStartDate' />
+        <ImageUploadField name='itemImage' />
       </form>
+      <div className='w-layout flex justify-between py-5'>
+        <Button variant='secondary' onClick={() => form.reset()}>
+          양식 지우기
+        </Button>
+        <div className='flex gap-3'>
+          <Button variant='outline' onClick={goPrevStep}>
+            뒤로
+          </Button>
+          <Button
+            variant='outline'
+            disabled={!form.formState.isValid}
+            type='submit'
+            onClick={() => form.handleSubmit(onSubmit)()}
+          >
+            제출
+          </Button>
+        </div>
+      </div>
     </Form>
   );
 };
