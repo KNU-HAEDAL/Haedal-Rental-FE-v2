befcd638c48c519849da8f8be4a9bd2c12139cbe
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-03-25 23:11:38 +0900

    fix(apply-form): padding 값 수정
    Co-authored-by: Minjoo Kim <itchu999@naver.com>

--- src/features/apply-form/components/form/ItemNameField.tsx
+++ src/features/apply-form/components/form/ItemNameField.tsx
@@ -21,7 +21,7 @@ export const ItemNameField = ({ name }: Props) => {
       control={form.control}
       name={name}
       render={({ field }) => (
-        <FormItem className='flex w-full flex-col px-5'>
+        <FormItem className='flex w-full flex-col px-3'>
           <FormDescription className='text-md'>
             대여 물품의 이름을 작성해주세요.
           </FormDescription>
