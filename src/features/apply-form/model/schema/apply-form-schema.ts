16d6ea8f08d9a1a2f6ea4e4ef1563f1bb92ecad7
Dobbymin <gmin990624@gmail.com>
2025-03-26 21:47:32 +0900

    fix(apply-form): schema 수정

--- src/features/apply-form/model/schema/apply-form-schema.ts
+++ src/features/apply-form/model/schema/apply-form-schema.ts
@@ -6,12 +6,13 @@ const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const ApplyFormSchema = z.object({
   itemName: z.string().min(1, { message: '물품 이름을 입력해주세요.' }),
   category: z.string().min(1, { message: '카테고리를 선택해주세요.' }),
-  rentalStartDate: z.string().refine((date) => date !== undefined, {
-    message: '대여 시작 날짜를 선택해주세요.',
-  }),
-  rentalEndDate: z.string(),
+  rentalStartDate: z
+    .string()
+    .min(1, { message: '대여 시작 날짜를 선택해주세요.' }),
+  rentalEndDate: z.string().min(1, { message: '반납 날짜를 선택해주세요.' }),
   itemImage: z
     .array(z.instanceof(File))
+    .min(1, { message: '이미지를 업로드해야 합니다.' })
     .refine((files) => files.every((file) => file.size <= MAX_FILE_SIZE), {
       message: '각 파일은 최대 5MB까지 업로드할 수 있습니다.',
     }),
