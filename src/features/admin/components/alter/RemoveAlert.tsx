55119092be7c16bb6d3700e9fd4137e2a6250a97
Dobbymin <gmin990624@gmail.com>
2025-03-27 11:18:49 +0900

    feat(admin): 물품 삭제 alert 구현

---
+++ src/features/admin/components/alter/RemoveAlert.tsx
@@ -0,0 +1,57 @@
+import { toast } from 'sonner';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  Button,
+} from '@/shared';
+
+type Props = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+export const RemoveAlert = ({ open, setOpen }: Props) => {
+  const promise = () =>
+    new Promise((resolve) =>
+      setTimeout(() => resolve({ name: 'Sonner' }), 1000),
+    );
+
+  const onClickRemove = () => {
+    toast.promise(promise, {
+      loading: '물품을 삭재하는 중...',
+      success: '물품이 삭제되었습니다.',
+      error: '물품 삭제를 실패했습니다.',
+    });
+    setOpen(false);
+  };
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className='w-sm border-none p-0'>
+        <AlertDialogHeader className='w-full'>
+          <AlertDialogTitle className='bg-haedal flex w-full justify-center rounded-t-md py-3 text-white'>
+            물품 삭제
+          </AlertDialogTitle>
+          <AlertDialogDescription className='flex justify-center py-2'>
+            선택한 물품을 정말 삭제하시겠습니까?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div className='flex w-full justify-center gap-3 pb-3'>
+          <AlertDialogCancel asChild>
+            <Button variant='outline' onClick={() => setOpen(false)}>
+              취소
+            </Button>
+          </AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button onClick={onClickRemove}>삭제</Button>
+          </AlertDialogAction>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
