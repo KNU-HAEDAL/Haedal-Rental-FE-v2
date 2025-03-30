259233eb83b1a9080bd2bfb2fb94d8b00ac1a86a
Dobbymin <gmin990624@gmail.com>
2025-03-30 22:01:31 +0900

    fix(items): 물품 리스트 hook 수정

--- src/features/items/hooks/useGetItemList.ts
+++ src/features/items/hooks/useGetItemList.ts
@@ -2,11 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 
 import { ITEM_LIST_PATH, ItemListParams, itemListAPI } from '../apis';
 
-export const getItemListQueryKey = [ITEM_LIST_PATH];
+export const getItemListQueryKey = (params: ItemListParams) => [
+  ITEM_LIST_PATH,
+  params,
+];
 
 export const useGetItemList = (params: ItemListParams) => {
   return useQuery({
-    queryKey: getItemListQueryKey,
+    queryKey: getItemListQueryKey(params),
     queryFn: () => itemListAPI(params),
   });
 };
