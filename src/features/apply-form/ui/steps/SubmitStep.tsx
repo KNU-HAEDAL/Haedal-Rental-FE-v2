c5600f7772b3ff5e7e21b7d2d4f7144904a1dc5d
joojjang <itchu999@naver.com>
2025-04-04 06:28:07 +0900

    refactor(SubmitStep): SUBMIT_MESSAGE도  렌더링 로직 리팩토링

--- src/features/apply-form/ui/steps/SubmitStep.tsx
+++ src/features/apply-form/ui/steps/SubmitStep.tsx
@@ -10,17 +10,33 @@ import {
 export const SubmitStep = () => {
   const navigate = useNavigate();
 
+  const replacePlaceholders = (message: string) => {
+    const replacedTag = message
+      .split(/(\{\{.*?\}\}|\*\*.*?\*\*)/g)
+      .map((part, index) => {
+        if (/^\*\*(.*?)\*\*$/.test(part)) {
+          return (
+            <b key={index} className='text-sun'>
+              {part.replace(/\*\*/g, '')}
+            </b>
+          );
+        }
+
+        return part;
+      });
+
+    return replacedTag;
+  };
+
   return (
     <>
       <ContentsContainer title='물품 대여 장부'>
         <DescriptionSection>
-          <div className='flex flex-col gap-2'>
-            {SUBMIT_MESSAGE.map((message, index) => (
-              <p key={index} className='whitespace-pre-line'>
-                {message}
-              </p>
-            ))}
-          </div>
+          {SUBMIT_MESSAGE.map((message, index) => (
+            <p key={index} className='leading-relaxed whitespace-pre-line'>
+              {replacePlaceholders(message)}
+            </p>
+          ))}
         </DescriptionSection>
       </ContentsContainer>
       <ButtonSection>
@@ -33,15 +49,8 @@ export const SubmitStep = () => {
 };
 
 const SUBMIT_MESSAGE = [
-  [
-    '물품 대여 장부를 작성 완료하었습니다.\n',
-    '원활한 물품 대여 관리를 위해 ',
-    <b key='submit-message-one'>반납 기한을 준수</b>,
-    '해주세요.',
-  ],
-  [
-    '대여한 물품은 해달의 소중한 자산입니다. ',
-    <b key='submit-message-one'>2차 공유 및 파손을 자제해주세요.</b>,
-  ],
-  ['관련 문의는 운영진에 개인적으로 연락해주시면 감사하겠습니다.'],
+  '물품 대여 장부를 작성 완료하었습니다.\n',
+  '원활한 물품 대여 관리를 위해 **반납 기한을 준수**해주세요.\n',
+  '대여한 물품은 해달의 소중한 자산입니다. **2차 공유 및 파손을 자제**해주세요.\n',
+  '관련 문의는 운영진에 개인적으로 연락해주시면 감사하겠습니다.',
 ];
