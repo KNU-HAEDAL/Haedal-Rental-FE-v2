1b7e3f855b71e3c1d07b7839e190724076783e92
joojjang <itchu999@naver.com>
2025-04-04 03:11:11 +0900

    feat(NoticeMessageBox): 메시지 데이터 안에 버튼 태그 넣음

--- src/features/apply-form/components/box/NoticeMessageBox.tsx
+++ src/features/apply-form/components/box/NoticeMessageBox.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/shared';
+
 export const NoticeMessageBox = () => {
   return (
     <>
@@ -5,9 +7,9 @@ export const NoticeMessageBox = () => {
         해달에서는 동아리 내의 공용 물품을 대여 장부 작성을 통해 무료로 대여하고
         있습니다. 대여 물품 관리를 위해 아래 사항을 꼭 지켜주세요.
       </p>
-      <ol className='mb-5 list-outside list-decimal gap-2 space-y-[1px] pl-5'>
+      <ol className='mb-5 list-outside list-decimal space-y-[1px] pl-5'>
         {NOTICE_MESSAGE.map((message, index) => (
-          <li key={index} className='leading-relaxed whitespace-pre-line'>
+          <li key={index} className='mb-1 leading-relaxed whitespace-pre-line'>
             {message}
           </li>
         ))}
@@ -17,42 +19,52 @@ export const NoticeMessageBox = () => {
 };
 
 const NOTICE_MESSAGE = [
-  [
-    '대여 가능 기간은 장부 작성일로부터 ',
-    <b className='text-sun' key='bold-1'>
-      최대 일주일
-    </b>,
-    '입니다.\n',
-    '연장을 원할 시 다시 대여해주세요.',
-  ],
-  [
-    "물품 반납 시 반드시 '나의 대여 물품 확인'에서 반납을 해주세요.\n",
-    '반납 절차 미완료 시 발생하는 모든 책임은 대여자에게 있습니다.',
-  ],
-  [
-    '반납 기한 초과 시 운영진이 대여자에게 연락을 진행합니다.\n',
-    <b className='text-sun' key='bold-2'>
-      1회
-    </b>,
-    ' 제재 시 ',
-    <b className='text-sun' key='bold-3'>
-      물품 대여가 일주일 간 제한
-    </b>,
-    '됩니다.\n',
-    <b className='text-sun' key='bold-4'>
-      3회
-    </b>,
-    ' 제재 시 ',
-    <b className='text-sun' key='bold-5'>
-      물품 대여가 영구적으로 제한
-    </b>,
-    '됩니다.',
-  ],
-  [
-    '대여 물품의 원활한 관리를 위해 운영진은 대여자의 정보를 확인할 수 있습니다.',
-  ],
-  [
-    '대여 중인 물품의 경우 대여할 수 없습니다.\n',
-    '전체 물품 및 대여 가능 물품은 아래 페이지에서 확인할 수 있습니다.',
-  ],
+  <>
+    대여 가능 기간은 장부 작성일로부터 <b className='text-sun'>최대 일주일</b>
+    입니다.
+    <br />
+    연장을 원할 시 다시 대여해주세요.
+  </>,
+  <>
+    물품 반납 시 반드시&nbsp;
+    <Button
+      variant='secondary'
+      className='mx-1 px-2 font-bold'
+      onClick={() => {
+        /* 이동 로직 */
+      }}
+    >
+      나의 대여 물품 확인
+    </Button>
+    에서 반납을 해주세요.
+    <br />
+    반납 절차 미완료 시 발생하는 모든 책임은 대여자에게 있습니다.
+  </>,
+  <>
+    반납 기한 초과 시 운영진이 대여자에게 연락을 진행합니다.
+    <br />
+    <b className='text-sun'>1회</b> 제재 시&nbsp;
+    <b className='text-sun'>물품 대여가 일주일 간 제한</b>됩니다.
+    <br />
+    <b className='text-sun'>3회</b> 제재 시&nbsp;
+    <b className='text-sun'>물품 대여가 영구적으로 제한</b>됩니다.
+  </>,
+  <>
+    대여 물품의 원활한 관리를 위해 운영진은 대여자의 정보를 확인할 수 있습니다.
+  </>,
+  <>
+    대여 중인 물품의 경우 대여할 수 없습니다.
+    <br />
+    전체 물품 및 대여 가능 물품은&nbsp;
+    <Button
+      variant='secondary'
+      className='mx-1 px-2 font-bold'
+      onClick={() => {
+        /* 이동 로직 */
+      }}
+    >
+      해달 물품 목록
+    </Button>
+    에서 확인할 수 있습니다.
+  </>,
 ];
