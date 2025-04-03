d3e30bd2d9020f713d92c8bb3fe4ea88e9e44384
joojjang <itchu999@naver.com>
2025-04-04 06:22:15 +0900

    refactor(SubmitStep): 신청 폼 제출 후 문구 부분 수정

--- src/features/apply-form/ui/steps/SubmitStep.tsx
+++ src/features/apply-form/ui/steps/SubmitStep.tsx
@@ -13,8 +13,8 @@ export const SubmitStep = () => {
   return (
     <>
       <ContentsContainer title='물품 대여 장부'>
-        <DescriptionSection className='items-center justify-center gap-3'>
-          <div className='flex flex-col gap-3'>
+        <DescriptionSection>
+          <div className='flex flex-col gap-2'>
             {SUBMIT_MESSAGE.map((message, index) => (
               <p key={index} className='whitespace-pre-line'>
                 {message}
@@ -25,7 +25,7 @@ export const SubmitStep = () => {
       </ContentsContainer>
       <ButtonSection>
         <Button variant='outline' onClick={() => navigate(RouterPath.MAIN)}>
-          첫 페이지로 이동
+          메인 페이지로 이동
         </Button>
       </ButtonSection>
     </>
@@ -35,12 +35,13 @@ export const SubmitStep = () => {
 const SUBMIT_MESSAGE = [
   [
     '물품 대여 장부를 작성 완료하었습니다.\n',
-    '원활한 물품 대여를 위해 ',
+    '원활한 물품 대여 관리를 위해 ',
     <b key='submit-message-one'>반납 기한을 준수</b>,
     '해주세요.',
   ],
-  ['대여한 물품은 해달의 소중한 재산입니다. 2차 공유 및 파손을 자제해주세요.'],
   [
-    '물품 대여 관련 문의는 해구르르에게 개인적으로 연락해주시면 감사하겠습니다.',
+    '대여한 물품은 해달의 소중한 자산입니다. ',
+    <b key='submit-message-one'>2차 공유 및 파손을 자제해주세요.</b>,
   ],
+  ['관련 문의는 운영진에 개인적으로 연락해주시면 감사하겠습니다.'],
 ];
