6373d998092a28236e8b8b80186659da397302f0
Dobbymin <gmin990624@gmail.com>
2025-04-07 17:40:24 +0900

    feat(admin): 물품 table type 정의

--- src/features/admin/model/types/item.type.ts
+++ src/features/admin/model/types/item.type.ts
@@ -1,10 +1,31 @@
-export type ItemList = {
-  header: { text: string; value: string }[];
-  body: {
-    type: string;
-    name: string;
-    status: string;
-    date: string;
-    borrower: string;
-  }[];
+import { GetItemListResponse } from '../../apis';
+
+export type HeaderData = {
+  text: string;
+  value: string;
 };
+
+export const ADMIN_ITEM_TABLE_HEADERS: HeaderData[] = [
+  {
+    text: '종류',
+    value: 'itemCategory',
+  },
+  {
+    text: '이름',
+    value: 'itemName',
+  },
+  {
+    text: '상태',
+    value: 'itemStatus',
+  },
+  {
+    text: '반납일',
+    value: 'returnDate',
+  },
+  {
+    text: '대여자',
+    value: 'rentalMemberName',
+  },
+];
+
+export type BodyData = GetItemListResponse;
