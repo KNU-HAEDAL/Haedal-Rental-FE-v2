0b4d02141ea0f847e83b7338c0593254f6d477bc
joojjang <itchu999@naver.com>
2025-04-09 23:38:37 +0900

    chore(login.api): 주석 삭제

--- src/features/login/apis/login.api.ts
+++ src/features/login/apis/login.api.ts
@@ -15,7 +15,6 @@ export const loginApi = async ({
 
   const accessToken = response.headers['authorization']?.replace('Bearer ', '');
   const refreshToken = response.headers['refresh-token'];
-  // Todo: response.data.userName, role도 받아야 함
   const userName = response.data.userName;
   const role = response.data.role;
 
