ed30f9671aa4864bbcad70efc9d97a1edac6f13b
joojjang <itchu999@naver.com>
2025-04-20 00:12:33 +0900

    fix(NavButtonSection): 반응형 너비 설정, 버튼을 button 태그로 변경

--- src/features/main/ui/section/NavButtonSection.tsx
+++ src/features/main/ui/section/NavButtonSection.tsx
@@ -4,15 +4,15 @@ import { NAVIGATE_BUTTONS_DATA } from '@/shared';
 
 export const NavButtonSection = () => {
   return (
-    <section className='grid h-auto w-[300px] grid-cols-2 place-content-center place-items-center gap-5 pb-3 sm:w-[380px]'>
+    <section className='grid w-full grid-cols-2 place-content-center place-items-center gap-3 px-3'>
       {NAVIGATE_BUTTONS_DATA.map((data) => (
-        <Link key={data.key} to={data.navigateTo}>
-          <div className='flex h-[125px] w-[140px] cursor-pointer flex-col items-center rounded-md border border-gray-200 py-2 shadow-md transition-all ease-in-out hover:-translate-y-1 hover:shadow-[5px_5px_20px_0px_rgba(0,0,0,0.10)] sm:w-[175px]'>
+        <Link key={data.key} to={data.navigateTo} className='w-full'>
+          <button className='flex w-full cursor-pointer flex-col items-center gap-2 rounded-md border border-gray-200 py-3 shadow-md transition-all ease-in-out hover:-translate-y-1 hover:shadow-[5px_5px_20px_0px_rgba(0,0,0,0.10)]'>
             <div className='flex flex-grow items-center'>
               <img src={data.icon} alt={data.title} width={60} height={50} />
             </div>
             <p className='text-sm font-bold sm:text-base'>{data.title}</p>
-          </div>
+          </button>
         </Link>
       ))}
     </section>
