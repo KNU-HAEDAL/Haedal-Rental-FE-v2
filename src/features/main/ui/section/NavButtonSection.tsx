606787fbcd322d4034d0999338f64eed5d7030d3
joojjang <itchu999@naver.com>
2025-04-20 00:15:44 +0900

    refactor(NavButtonSection): button에 navigate 처리

--- src/features/main/ui/section/NavButtonSection.tsx
+++ src/features/main/ui/section/NavButtonSection.tsx
@@ -1,19 +1,24 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { NAVIGATE_BUTTONS_DATA } from '@/shared';
 
 export const NavButtonSection = () => {
+  const navigate = useNavigate();
+
   return (
     <section className='grid w-full grid-cols-2 place-content-center place-items-center gap-3 px-3'>
       {NAVIGATE_BUTTONS_DATA.map((data) => (
-        <Link key={data.key} to={data.navigateTo} className='w-full'>
-          <button className='flex w-full cursor-pointer flex-col items-center gap-2 rounded-md border border-gray-200 py-3 shadow-md transition-all ease-in-out hover:-translate-y-1 hover:shadow-[5px_5px_20px_0px_rgba(0,0,0,0.10)]'>
-            <div className='flex flex-grow items-center'>
-              <img src={data.icon} alt={data.title} width={60} height={50} />
-            </div>
-            <p className='text-sm font-bold sm:text-base'>{data.title}</p>
-          </button>
-        </Link>
+        <button
+          key={data.key}
+          type='button'
+          onClick={() => navigate(data.navigateTo)}
+          className='flex w-full cursor-pointer flex-col items-center gap-2 rounded-md border border-gray-200 py-3 shadow-md transition-all ease-in-out hover:-translate-y-1 hover:shadow-[5px_5px_20px_0px_rgba(0,0,0,0.10)]'
+        >
+          <div className='flex flex-grow items-center'>
+            <img src={data.icon} alt={data.title} width={60} height={50} />
+          </div>
+          <p className='text-sm font-bold sm:text-base'>{data.title}</p>
+        </button>
       ))}
     </section>
   );
