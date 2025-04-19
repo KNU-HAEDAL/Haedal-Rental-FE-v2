bd59015858b492c3040cf9ead62221df5f0e81e7
joojjang <itchu999@naver.com>
2025-04-20 00:45:58 +0900

    fix(auth): 변경된 index.css 참조하여 CSS 수정

--- src/widgets/wrapper/PageWrapper.tsx
+++ src/widgets/wrapper/PageWrapper.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { RouterPath, SimpleLogo, SymbolLogo, cn } from '@/shared';
+import { RouterPath, SimpleLogo, SymbolLogo } from '@/shared';
 
 import { AuthButtonContainer } from '../containers';
 
@@ -10,17 +11,26 @@ type Props = {
 
 export const PageWrapper = ({ children }: Props) => {
   const location = useLocation();
+  const path = location.pathname;
 
-  const isAuthPage =
-    location.pathname === RouterPath.SIGNUP ||
-    location.pathname === RouterPath.LOGIN;
+  const isAuthPage = path === RouterPath.SIGNUP || path === RouterPath.LOGIN;
+
+  useEffect(() => {
+    const body = document.body;
+
+    // body에 클래스 붙여 배경 스타일 적용
+    if (path.startsWith('/login') || path.startsWith('/signup')) {
+      body.className = 'page-auth';
+    } else {
+      body.className = 'page-main';
+    }
+  }, [path]);
 
   return (
     <main
-      className={cn(
-        'flex h-screen w-full flex-col items-center justify-start text-center',
-        isAuthPage && 'bg-haedal text-white',
-      )}
+      className={
+        'flex h-screen w-full flex-col items-center justify-start text-center'
+      }
     >
       {isAuthPage ? (
         <>
